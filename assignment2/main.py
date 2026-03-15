887fab79956f0f861120c38c5be2d333fd6f4b62
skarout1 <sadikkarout@gmail.com>
2026-03-15 17:58:32 -0400

    assignment2 edited in seperate branch abd commited

--- assignment2/main.py
+++ assignment2/main.py
@@ -6,8 +6,8 @@ from cashier import Cashier
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = resources 
-cashier_instance = recipes
+sandwich_maker_instance = SandwichMaker
+cashier_instance = Cashier
 
 
 
@@ -15,24 +15,26 @@ cashier_instance = recipes
 def main():
     while True:
         action = input("What would you like? (small/ medium/ large/ off/ report): ")
-        sand_type = {action: SandwichMaker(action)}
+        sandwich_maker_instance = {action: SandwichMaker(action)}
+        cashier_instance = {action: Cashier()}
         if action == "off":
             print("The machine has been turned off!")
             break
         elif action == "report":
             print(resources)
         else:
-            sand_type[action] = SandwichMaker(sand_type[action])
-            if sand_type[action].check_resources(recipes[action]["ingredients"]):
-                money = sand_type[action].process_coins()
-                if money < 0:
-                    break
-                sand_type[action].transaction_result(money, recipes[action]["cost"])
-                sand_type[action].make_sandwich(recipes[action]["ingredients"], resources)
+            sandwich_maker_instance[action] = SandwichMaker(sandwich_maker_instance[action])
+            if sandwich_maker_instance[action].check_resources(recipes[action]["ingredients"]):
+                money = cashier_instance[action].process_coins()
+                if money >= data.recipes[action]["cost"]:
+                    cashier_instance[action].transaction_result(money, recipes[action]["cost"])
+                    sandwich_maker_instance[action].make_sandwich(recipes[action]["ingredients"], resources)
+                else:
+                    cashier_instance[action].transaction_result(money, recipes[action]["cost"])
+                    
             else:
                 print("There are not enough resources.")
 
 if __name__=="__main__":
     main()
-
 
--- assignment2/sandwich_maker.py
+++ assignment2/sandwich_maker.py
@@ -1,11 +1,14 @@
 
+import data
+from cashier import Cashier
+
 class SandwichMaker:
     def __init__(self, resources):
-        self.machine_resources = resources
+        self.machine_resources = data.resources
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        if resources["bread"] >= ingredients["bread"] and resources["ham"] >= ingredients["ham"] and resources["cheese"] >= ingredients["cheese"]:
+        if self.machine_resources["bread"] >= ingredients["bread"] and self.machine_resources["ham"] >= ingredients["ham"] and self.machine_resources["cheese"] >= ingredients["cheese"]:
             return True
         else:
             return False
@@ -14,4 +17,4 @@ class SandwichMaker:
         order_ingredients["bread"] -= sandwich_size["bread"]
         order_ingredients["ham"] -= sandwich_size["ham"]
         order_ingredients["cheese"] -= sandwich_size["cheese"]
-        return print(f"{action} sandwich is ready. Bon appetit!")
+        return print(f"sandwich is ready. Bon appetit!")
